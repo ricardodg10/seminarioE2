ec8ec3b6c6c833f108174f9fab7be131cba60f9e
Ricardo <ricardo.diazgo@alumnos.uv.cl>
2025-04-18 21:22:42 -0400

    Arreglando estructuras de algoritmos (APO binario casi terminado)

--- nativos_binarios/apo-bi/apo-bi.py
+++ nativos_binarios/apo-bi/apo-bi.py
@@ -1,157 +1,210 @@
-import random       # para las variables aleatorias del algoritmo
-import time         # para medir el tiempo de ejecución
-import math         # para funciones matemáticas como logaritmo y tangente
+import random
+import time
+import math
 
-# función lévy flight para saltos aleatorios en el espacio de búsqueda
+# -----------------------------------------------
+# FUNCIONES AUXILIARES
+# -----------------------------------------------
+
+def leer_vertices(ruta):
+    with open(ruta, 'r') as file:
+        lines = file.readlines()
+        num_vertices, _, _ = map(int, lines[1].split())
+    return num_vertices
+
+def leer_aristas(ruta):
+    aristas = []
+    with open(ruta, 'r') as file:
+        lines = file.readlines()
+        for line in lines[2:]:
+            v1, v2 = map(int, line.split())
+            aristas.append((v1, v2))  # base 1
+    return aristas
+
+def s1(v):
+    return 1 / (1 + math.exp(-v))
+
+def binarizar_sigmoide(v):
+    return 1 if random.random() < s1(v) else 0
+
 def levy_flight(dimension):
-    beta = 1.5 # 0 < beta <= 2
-
+    beta = 1.5
     sigma_u = (math.gamma(1 + beta) * math.sin(math.pi * beta / 2) /
-             (math.gamma((1 + beta) / 2) * beta * 2**((beta - 1) / 2)))**(1 / beta)
+               (math.gamma((1 + beta) / 2) * beta * 2 ** ((beta - 1)/2)))**(1 / beta)
     sigma_v = 1
-
     U = [random.gauss(0, sigma_u) for _ in range(dimension)]
     V = [random.gauss(0, sigma_v) for _ in range(dimension)]
+    return [u / abs(v) ** (1 / beta) for u, v in zip(U, V)]
 
-    step_lenght = [u / abs(v)**(1 / beta) for u, v in zip(U, V)] # zip para iterar dentro del mismo "for" los valores de "U" y de "V"
-    return step_lenght
+def funcion_objetivo(solucion):
+    return sum(solucion)
 
-# clase puffin
+def reparar_solucion(solucion, aristas):
+    for v1, v2 in aristas:
+        if solucion[v1 - 1] == 0 and solucion[v2 - 1] == 0:
+            solucion[random.choice([v1 - 1, v2 - 1])] = 1
+    return solucion
+
+# -----------------------------------------------
+# CLASE PUFFIN (INDIVIDUO)
+# -----------------------------------------------
 class Puffin:
-    def __init__(self, dimension, LB, UB):
-        self.dimension = dimension      # número de dimensiones del problema
-        self.lower_bound = LB           # límite inferior del espacio de búsqueda
-        self.upper_bound = UB           # límite superior del espacio de búsqueda
+    def __init__(self, dimension, LB, UB, aristas):
+        self.dimension = dimension
+        self.lower_bound = LB
+        self.upper_bound = UB
+        self.aristas = aristas
+        self.posicion_real = [random.uniform(LB, UB) for _ in range(dimension)]
+        self.posicion = [binarizar_sigmoide(v) for v in self.posicion_real]
+        self.posicion = reparar_solucion(self.posicion, aristas)
+        self.fitness = funcion_objetivo(self.posicion)
 
-        # inicializar posición aleatoria
-        self.posicion = [random.uniform(LB, UB) for _ in range(dimension)]
-        self.valor = float('inf')       # valor de la función objetivo (a minimizar)
+    def evaluar(self):
+        self.fitness = funcion_objetivo(self.posicion)
+        return self.fitness
 
-    def evaluar(self, funcion_objetivo):
-        # evalúa el valor de la función objetivo en la posición actual
-        self.valor = funcion_objetivo(self.posicion)
-        return self.valor
+    def ajustar_limites(self, vector):
+        return [max(self.lower_bound, min(x, self.upper_bound)) for x in vector]
 
-# clase de algoritmo APO (Arctic Puffin Optimization)
+    def actualizar_desde_vector_real(self, nuevo_vector):
+        self.posicion_real = self.ajustar_limites(nuevo_vector)
+        self.posicion = [binarizar_sigmoide(v) for v in self.posicion_real]
+        self.posicion = reparar_solucion(self.posicion, self.aristas)
+        self.fitness = funcion_objetivo(self.posicion)
+
+    # Estrategia aérea: evitar depredadores
+    def evitar_dep(self, X_r, L, R):
+        return [x_i + (x_i - x_r) * l + R for x_i, x_r, l in zip(self.posicion_real, X_r, L)]
+
+    # Estrategia aérea: picada
+    def picada(self, Y, S):
+        return [y * S for y in Y]
+
+    # Estrategia subacuática: recolección cooperativa
+    def recoleccion(self, X2, X3, F, L1=None):
+        if L1:
+            return [x1 + F * l1 * (x2 - x3) for x1, x2, x3, l1 in zip(self.posicion_real, X2, X3, L1)]
+        else:
+            return [x1 + F * (x2 - x3) for x1, x2, x3 in zip(self.posicion_real, X2, X3)]
+
+    # Estrategia subacuática: intensificación
+    def intensificar(self, W, f):
+        return [w * (1 + f) for w in W]
+
+    # Estrategia subacuática: evasión
+    def evasion(self, X_r, modo="levy", L2=None, beta=None, F=0.5):
+        if modo == "levy":
+            return [x1 + F * l * (x1 - x2) for x1, x2, l in zip(self.posicion_real, X_r, L2)]
+        else:
+            return [x1 + beta * (x1 - x2) for x1, x2 in zip(self.posicion_real, X_r)]
+
+# -----------------------------------------------
+# CLASE APO
+# -----------------------------------------------
 class APO:
-    def __init__(self, funcion_objetivo, num_puffins, dimension, lower_bound, upper_bound, max_iter):
-        self.funcion_objetivo = funcion_objetivo
+    def __init__(self, num_puffins, dimension, LB, UB, max_iter, aristas):
         self.num_puffins = num_puffins
         self.dimension = dimension
-        self.lower_bound = lower_bound
-        self.upper_bound = upper_bound
+        self.lower_bound = LB
+        self.upper_bound = UB
         self.max_iter = max_iter
-
-        # crear la población inicial de puffins
-        self.poblacion = [Puffin(dimension, lower_bound, upper_bound) for _ in range(num_puffins)]
-
-        # inicializar mejor solución global
+        self.aristas = aristas
+        self.poblacion = [Puffin(dimension, LB, UB, aristas) for _ in range(num_puffins)]
         self.mejor_global = self.poblacion[0].posicion[:]
-        self.mejor_valor = float('inf')
-
-    def ajustar_limites(self, posicion):
-        # asegura que cada valor de la posición esté dentro de los límites permitidos
-        return [max(self.lower_bound, min(x, self.upper_bound)) for x in posicion]
+        self.mejor_valor = self.poblacion[0].fitness
 
     def optimizar(self):
-        tiempo_inicio = time.time()  # medir tiempo de inicio
+        tiempo_inicio = time.time()
 
         for t in range(1, self.max_iter + 1):
-            # factor de conversión de comportamiento B para determinar la estrategia (aire o agua)
             B = 2 * math.log(1 / random.random()) * (1 - (t / self.max_iter))
-            C = 0.5  # valor umbral, definido en la teoría
+            C = 0.5
+            candidatos = []
 
-            posiciones_candidatas = []
-
-            # Fase aérea (exploración global)
-            if B > C:  
+            if B > C:
+                # Fase aérea
                 for i, puffin in enumerate(self.poblacion):
-                    # estrategia 1: evadir depredadores
-                    X_i = puffin.posicion
-                    r = random.choice([j for j in range(self.num_puffins) if j != i])       # indice aleatorio de un individuo (excluye el actual)
-                    X_r = self.poblacion[r].posicion                                        # posición del individuo aleatorio
-                    L = levy_flight(self.dimension)                                         # componente de levy flight dentro de la dimension del problema
-                    alpha = random.gauss(0, 1)                                              # random [0,1], distribución normal
-                    R = round(0.5 * (0.5 + random.random()) * alpha)                        # componente R
-                    Y_i = [x_i + (x_i - x_r) * l + R for x_i, x_r, l in zip(X_i, X_r, L)]   # posicion nueva al evadir depredadores
+                    r = random.choice([j for j in range(self.num_puffins) if j != i])
+                    X_r = self.poblacion[r].posicion_real
+                    L = levy_flight(self.dimension)
+                    alpha = random.gauss(0, 1)
+                    R = round(0.5 * (0.5 + random.random()) * alpha)
 
-                    # estrategia 2: depredación en picada
-                    S = math.tan((random.random() - 0.5) * math.pi)                         # coeficiente de velocidad
-                    Z_i = [y * S for y in Y_i]                                              # nueva posición luego de la fase aérea
+                    Y = puffin.evitar_dep(X_r, L, R)
+                    S = math.tan((random.random() - 0.5) * math.pi)
+                    Z = puffin.picada(Y, S)
 
-                    # unión de las soluciones
-                    posiciones_candidatas.append((self.ajustar_limites(Y_i), self.funcion_objetivo(Y_i)))
-                    posiciones_candidatas.append((self.ajustar_limites(Z_i), self.funcion_objetivo(Y_i)))
-            
-            # Fase subacuática (explotación local)
-            else:  
-                for i in range(self.num_puffins):
-                    r1_1, r2_1, r3_1 = random.sample([j for j in range(self.num_puffins) if j != i], 3)                                     # se definen los índices de tres individuos distintos al actual (sample: para que los escogidos no sean repetidos)
-                    X_r1_1, X_r2_1, X_r3_1 = self.poblacion[r1_1].posicion, self.poblacion[r2_1].posicion, self.poblacion[r3_1].posicion    # se obtienen las posiciones de los individuos aleatorios
-                    F = 0.5                                                                                                                 # factor cooperativo (valor por teoría)
-                    L1 = levy_flight(self.dimension)                                                                                        # componente de levy flight dentro de la dimension del problema
+                    # evaluar Y y Z como candidatos
+                    for vec in [Y, Z]:
+                        temp = Puffin(self.dimension, self.lower_bound, self.upper_bound, self.aristas)
+                        temp.actualizar_desde_vector_real(vec)
+                        candidatos.append((temp.posicion, temp.fitness))
+            else:
+                # Fase subacuática
+                for i, puffin in enumerate(self.poblacion):
+                    r2, r3 = random.sample([j for j in range(self.num_puffins) if j != i], 2)
+                    X2 = self.poblacion[r2].posicion_real
+                    X3 = self.poblacion[r3].posicion_real
+                    F = 0.5
 
-                    # estrategia 1: recolección de comida con la población
-
-                    ## explora el entorno en cooperación con otros individuos
                     if random.random() >= 0.5:
-                        W_i = [x_r1 + F * l1 * (x_r2 - x_r3) for x_r1, x_r2, x_r3, l1 in zip(X_r1_1, X_r2_1, X_r3_1, L1)]   # posición nueva al explorar le entorno en cooperación con otros individuos        
-
-                    ## sigue a otros individuos y se une a un grupo más ventajoso
+                        L1 = levy_flight(self.dimension)
+                        W = puffin.recoleccion(X2, X3, F, L1)
                     else:
-                        W_i = [x_r1 + F * (x_r2 - x_r3) for x_r1, x_r2, x_r3 in zip(X_r1_1, X_r2_1, X_r3_1)]                # posición nueva al seguir al grupo más ventajoso
+                        W = puffin.recoleccion(X2, X3, F)
 
-                    # estrategia 2: búsqueda intensificada (cambio de posición en el agua por falta de alimento)
-                    f = 0.1 * (random.random() - 1) * (1 - (t / self.max_iter))                                             # factor adaptativo del frailencillo en el agua
-                    Y_i = [w_i * (1 + f) for w_i in W_i]                                                                    # nueva posición en el agua
+                    f = 0.1 * (random.random() - 1) * (1 - (t / self.max_iter))
+                    Y = puffin.intensificar(W, f)
 
-                    # estrategia 3: evadir depredadores
-
-                    r1_2, r2_2 = random.sample([j for j in range(self.num_puffins) if j != i], 2)                               # se definen los índices de dos individuos distintos al actual (sample: para que los escogidos no sean repetidos)
-                    X_r1_2, X_r2_2 = self.poblacion[r1_2].posicion, self.poblacion[r2_2].posicion                               # se obtienen las posiciones de los individuos aleatorios
+                    r4 = random.choice([j for j in range(self.num_puffins) if j != i])
+                    X_r = self.poblacion[r4].posicion_real
 
                     if random.random() >= 0.5:
-                        L2 = levy_flight(self.dimension)                                                                        # componente de levy flight dentro de la dimension del problema
-                        Z_i = [x_r1_2 + F * l2 * (x_r1_2 - x_r2_2) for x_r1_2, x_r2_2, l2 in zip(X_r1_2, X_r2_2, L2)]           # nueva posición luego de evadir depredadores
+                        L2 = levy_flight(self.dimension)
+                        Z = puffin.evasion(X_r, "levy", L2=L2, F=F)
                     else:
                         beta = random.uniform(0, 1)
-                        Z_i = [x_r1_2 + beta * (x_r1_2 - x_r2_2) for x_r1_2, x_r2_2 in zip(X_r1_2, X_r2_2)]                     # nueva posición luego de evadir depredadores
+                        Z = puffin.evasion(X_r, "beta", beta=beta)
 
-                    # union de las soluciones
-                    posiciones_candidatas.append((self.ajustar_limites(W_i), self.funcion_objetivo(W_i)))
-                    posiciones_candidatas.append((self.ajustar_limites(Y_i), self.funcion_objetivo(Y_i)))
-                    posiciones_candidatas.append((self.ajustar_limites(Z_i), self.funcion_objetivo(Z_i)))
-            
-            # se ordenan las soluciones desde el menor al mayor fitness
-            posiciones_candidatas.sort(key=lambda x: x[1])
+                    for vec in [W, Y, Z]:
+                        temp = Puffin(self.dimension, self.lower_bound, self.upper_bound, self.aristas)
+                        temp.actualizar_desde_vector_real(vec)
+                        candidatos.append((temp.posicion, temp.fitness))
 
-            # se guardan las mejores posiciones y se reemplazan en la población
+            # seleccionar mejores soluciones
+            candidatos.sort(key=lambda x: x[1])
             for i in range(self.num_puffins):
-                self.poblacion[i].posicion = posiciones_candidatas[i][0]
+                self.poblacion[i].posicion = candidatos[i][0]
+                self.poblacion[i].posicion_real = candidatos[i][0]
 
-            # luego de haber actualizado la población completa
             for puffin in self.poblacion:
-                valor = puffin.evaluar(self.funcion_objetivo)
-
+                valor = funcion_objetivo(puffin.posicion)
                 if valor < self.mejor_valor:
                     self.mejor_valor = valor
                     self.mejor_global = puffin.posicion[:]
 
-            print(f"Iteración {t} - Mejor solución {self.mejor_global} - Mejor valor = {self.mejor_valor}")
+            print(f"Iteración {t} - Vértices cubiertos (activos): {sum(self.mejor_global)}")
 
-        tiempo_total = time.time() - tiempo_inicio  # tiempo final
+        tiempo_total = time.time() - tiempo_inicio
         return self.mejor_global, self.mejor_valor, tiempo_total
 
-
-# main
+# -----------------------------------------------
+# BLOQUE PRINCIPAL
+# -----------------------------------------------
 if __name__ == "__main__":
-    def funcion_objetivo(pos):
-        # ejemplo de función objetivo: esfera
-        return sum(x for x in pos)
+    ruta_archivo = "C:/Users/ricar/OneDrive/Escritorio/seminario/benchmark/C125-9.mtx"
+    num_vertices = leer_vertices(ruta_archivo)
+    aristas = leer_aristas(ruta_archivo)
 
-    apo = APO(funcion_objetivo, num_puffins=5, dimension=2, lower_bound=-10, upper_bound=10, max_iter=10)
-    mejor_pos, mejor_valor, tiempo = apo.optimizar()
+    tamanio_poblacion = 5
+    lower_bound = -6
+    upper_bound = 6
+    max_iteraciones = 100
 
-    print("\nMejor posición encontrada:", mejor_pos)
-    print("Valor mínimo encontrado:", mejor_valor)
+    apo = APO(tamanio_poblacion, num_vertices, lower_bound, upper_bound, max_iteraciones, aristas)
+    mejor_sol, mejor_valor, tiempo = apo.optimizar()
+
+    print("\nMejor solución encontrada (binaria):", mejor_sol)
+    print("Vértices seleccionados:", [i + 1 for i, v in enumerate(mejor_sol) if v == 1])
+    print("Valor de la función objetivo:", mejor_valor)
     print("Tiempo de ejecución:", tiempo, "[s]")
